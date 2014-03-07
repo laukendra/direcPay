8aaf90d3d4cf6bd3c4bdba1f41bacfd55b4f487e
laukendra <laukendra@intelligrape.com>
2014-03-07 18:19:15 +0530

    Added src

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ SSH Url: git@github.com:laukendra/direcPay.git
 ===================================================
 -------------------Config Required------------------
 ===================================================
+Note: Please put following config in your Config.groovy file
+
+baseURL = http://localhost:8080/DirecPayTest/
+grails.plugins.direcPay.return.transaction.details.URL = "${baseURL}/paymentDetails"
+grails.plugins.direcPay.loadingText = "Loading..."
+grails.plugins.direcPay.operatingMode = "DOM"
+grails.plugins.direcPay.collaborator = "TOML"
+grails.plugins.direcPay.encryption.secretKey = "qcAHa6tt8s0l5NN7UWPVAQ=="
+grails.plugins.direcPay.merchantId = "200904281000001"
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -18,27 +18,3 @@ log4j = {
             'net.sf.ehcache.hibernate'
 }
 
-direcPay.URL = "https://test.timesofmoney.com/direcpay/secure/dpMerchantPayment.jsp"
-direcPay.pull.transaction.details.URL = "https://test.timesofmoney.com/direcpay/secure/dpPullMerchAtrnDtls.jsp"
-direcPay.return.transaction.details.URL = "http://localhost:8080/direcPay/paymentDetails"
-direcPay.loadingText = "Loading..."
-direcPay.operatingMode = "DOM"
-
-environments {
-    development {
-        direcPay.encryption.secretKey = "qcAHa6tt8s0l5NN7UWPVAQ=="
-        direcPay.merchantId = "200904281000001"
-        direcPay.collaborator = "TOML"
-    }
-    test {
-        direcPay.encryption.secretKey = "qcAHa6tt8s0l5NN7UWPVAQ=="
-        direcPay.merchantId = "200904281000001"
-        direcPay.collaborator = "TOML"
-    }
-    staging {
-        direcPay.collaborator = "DirecPay"
-    }
-    production {
-        direcPay.collaborator = "DirecPay"
-    }
-}
